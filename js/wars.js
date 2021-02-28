fd313c6b8e3448b96d89dc844cb1d0a97d96c2cb
zvlzvl <76951647+zvlzvl@users.noreply.github.com>
2021-02-28 14:38:50 +0200

    replace letter

--- js/wars.js
+++ js/wars.js
@@ -446,4 +446,152 @@ function chuckPushUps(string) {
 console.log(chuckPushUps('1 "Chuck" 10 "Stop that!" 11 "Your vest looks stupid" 100 101 110'));
 
 
+function solution(number){ 
+  let arr1 = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  let arr2 = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"] 
+  let newStr = '';
+    for (let i = 0; i < arr1.length; i++) {
+      while (arr1[i]<= number){
+      newStr += arr2[i];
+      number -= arr1[i];
+    }  
+  }
+  return newStr
+}
 
+console.log(solution(125));
+
+
+
+  function solutio(roman) {
+      let arr1 = [900, 1000, 400, 500, 90, 100, 40, 50, 9, 10, 4, 5, 1];
+      let arr2 = ['CM', 'M', 'CD', 'D', 'XC', 'C', 'XL', 'L', 'IX', 'X', 'IV', 'V', 'I'];
+      let suma = 0;
+        for (let i = 0; i < arr2.length; i++) {
+          if (roman.includes(arr2[i])){ 
+          suma += arr1[i];
+          roman = roman.replace(arr2[i],'');
+           } 
+       } 
+        if (roman === ''){
+          return suma
+       }
+      return suma+solutio(roman)
+    } 
+  
+  
+ console.log(solutio('MDCLXVI'))
+
+
+ let a1 = [121, 144, 19, 161, 19, 144, 19, 11];
+ let a2 = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19];
+ function comp(array1, array2){
+   const str1 = array1.sort().join('');
+   const str2 = array2.map(x => Math.sqrt(x)).sort().join('');
+  return str1 === str2
+  }
+  console.log(comp(a1, a2));
+
+
+  function solve(s, k){
+    while (k > 0){
+      if (s.includes('a')){
+        s=s.replace('a', '');
+        k-=1;
+      }else if (s.includes('b')){
+        s=s.replace('b', '');
+        k-=1;
+      }else if (s.includes('c')){
+        s=s.replace('c', '');
+        k-=1;
+      }else if (s.includes('d')){
+        s=s.replace('d', '');
+        k-=1;
+      }else if (s.includes('e')){
+        s=s.replace('e', '');
+        k-=1;
+      }else if (s.includes('f')){
+        s=s.replace('f', '');
+        k-=1;
+      }else if (s.includes('g')){
+        s=s.replace('g', '');
+        k-=1;
+      }else if (s.includes('h')){
+        s=s.replace('h', '');
+        k-=1;
+      }else if (s.includes('i')){
+        s=s.replace('i', '');
+        k-=1;
+      }else if (s.includes('j')){
+        s=s.replace('j', '');
+        k-=1;
+      }else if (s.includes('k')){
+        s=s.replace('k', '');
+        k-=1;
+      }else if (s.includes('l')){
+        s=s.replace('l', '');
+        k-=1;
+      }else if (s.includes('m')){
+        s=s.replace('m', '');
+        k-=1;
+      }else if (s.includes('n')){
+        s=s.replace('n', '');
+        k-=1;
+      }else if (s.includes('o')){
+        s=s.replace('o', '');
+        k-=1;
+      }else if (s.includes('p')){
+        s=s.replace('p', '');
+        k-=1;
+      }else if (s.includes('q')){
+        s=s.replace('q', '');
+        k-=1;
+      }else if (s.includes('r')){
+        s=s.replace('r', '');
+        k-=1;
+      }else if (s.includes('s')){
+        s=s.replace('s', '');
+        k-=1;
+      }else if (s.includes('t')){
+        s=s.replace('t', '');
+        k-=1;
+      }else if (s.includes('u')){
+        s=s.replace('u', '');
+        k-=1;
+      }else if (s.includes('v')){
+        s=s.replace('v', '');
+        k-=1;
+      }else if (s.includes('w')){
+        s=s.replace('w', '');
+        k-=1;
+      }else if (s.includes('x')){
+        s=s.replace('x', '');
+        k-=1;
+      }else if (s.includes('y')){
+        s=s.replace('y', '');
+        k-=1;
+      }else if (s.includes('z')){
+        s=s.replace('z', '');
+        k-=1;
+    }else {
+      return s
+    }
+    }
+        return s
+     } 
+     console.log(solve('abracadabra', 1));
+     
+     
+     function solve(s, k){
+      const abc = "abcdefghijklmnopqrstuvwxyz"
+      
+      for (let i=0; i<abc.length; i+=1) {
+        while(s.includes(abc[i]) && k > 0) {
+          s = s.replace(abc[i], "")
+          k -= 1
+        }
+      }
+      
+      return s
+    }
+    console.log(solve('abracadabra', 1));
